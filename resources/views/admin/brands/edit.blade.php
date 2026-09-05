5de5cc8b073f33478d3fd7eb39274ef78feadcb7
Sinet Tork <156560309+sinettork@users.noreply.github.com>
2026-09-05 10:01:49 +0700

    Remove duplicate Edit Brand heading

--- resources/views/admin/brands/edit.blade.php
+++ resources/views/admin/brands/edit.blade.php
@@ -3,11 +3,6 @@
 @section('title', 'Edit Brand')
 
 @section('content')
-<div class="mb-4">
-    <h1 class="h3 mb-1">Edit Brand</h1>
-    <p class="text-muted mb-0">Update {{ $brand->name }}.</p>
-</div>
-
 <form method="POST" action="{{ route('admin.brands.update', $brand) }}" enctype="multipart/form-data">
     @method('PUT')
     @include('admin.brands._form', ['brand' => $brand])
