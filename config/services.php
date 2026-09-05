9ba0b0e567072facb7a4342b8ff9db72aca12ad7
Sinet Tork <156560309+sinettork@users.noreply.github.com>
2026-09-05 09:10:44 +0700

    Configure Brandfetch client

--- config/services.php
+++ config/services.php
@@ -2,18 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Resend, Postmark, AWS, and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
@@ -33,6 +21,10 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+    ],
+
+    'brandfetch' => [
+        'client_id' => env('BRANDFETCH_CLIENT_ID'),
     ],
 
 ];
