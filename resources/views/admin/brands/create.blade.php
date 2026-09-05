ae409bc0a4d53f24b1474d7f3dbb6094b8732a8b
Sinet Tork <156560309+sinettork@users.noreply.github.com>
2026-09-05 10:01:46 +0700

    Remove duplicate Add Brand heading

--- resources/views/admin/brands/create.blade.php
+++ resources/views/admin/brands/create.blade.php
@@ -3,11 +3,6 @@
 @section('title', 'Add Brand')
 
 @section('content')
-<div class="mb-4">
-    <h1 class="h3 mb-1">Add Brand</h1>
-    <p class="text-muted mb-0">Create a phone manufacturer.</p>
-</div>
-
 <form method="POST" action="{{ route('admin.brands.store') }}" enctype="multipart/form-data">
     @include('admin.brands._form')
 </form>
