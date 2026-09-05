71afc1c1747a5a1ce94d8f40fca8e40186a704d0
Sinet Tork <156560309+sinettork@users.noreply.github.com>
2026-09-05 10:58:27 +0700

    Update brand page data for navigation, editorial, filters, and sorting

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\NewsPost;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
@@ -11,16 +12,91 @@ class BrandController extends Controller
     {
         $query = $brand->devices()
             ->with('brand')
-            ->orderByDesc('release_date');
+            ->withCount('specs');
 
         if ($request->filled('status')) {
             $query->where('status', $request->query('status'));
         }
 
-        $devices = $query->paginate(12)->withQueryString();
+        if ($request->boolean('card_slot')) {
+            $query->whereHas('specs', function ($specs) {
+                $specs->where('spec_key', 'like', '%card slot%');
+            });
+        }
+
+        if ($request->boolean('jack_3_5mm')) {
+            $query->whereHas('specs', function ($specs) {
+                $specs->where(function ($where) {
+                    $where->where('spec_key', 'like', '%3.5mm%')
+                        ->orWhere('spec_value', 'like', '%3.5mm%');
+                });
+            });
+        }
+
+        if ($request->boolean('esim')) {
+            $query->whereHas('specs', function ($specs) {
+                $specs->where(function ($where) {
+                    $where->where('spec_key', 'like', '%esim%')
+                        ->orWhere('spec_value', 'like', '%esim%');
+                });
+            });
+        }
+
+        if ($request->boolean('foldable')) {
+            $query->where(function ($devices) {
+                $devices->where('name', 'like', '%fold%')
+                    ->orWhereHas('specs', function ($specs) {
+                        $specs->where(function ($where) {
+                            $where->where('spec_key', 'like', '%form factor%')
+                                ->orWhere('spec_value', 'like', '%fold%');
+                        });
+                    });
+            });
+        }
+
+        $sort = $request->query('sort', 'new');
+
+        if ($sort === 'popular') {
+            $query->orderByDesc('specs_count')->orderByDesc('release_date');
+        } else {
+            $query->orderByDesc('release_date');
+        }
+
+        $devices = $query->paginate(15)->withQueryString();
 
         $deviceCount = $brand->devices()->count();
 
-        return view('brands.show', compact('brand', 'devices', 'deviceCount'));
+        $brands = Brand::query()
+            ->orderBy('name')
+            ->get();
+
+        $brandNews = NewsPost::query()
+            ->whereNotNull('published_at')
+            ->where(function ($news) use ($brand) {
+                $news->where('title', 'like', '%' . $brand->name . '%')
+                    ->orWhere('body', 'like', '%' . $brand->name . '%');
+            })
+            ->orderByDesc('published_at')
+            ->first();
+
+        $editorialPost = $brandNews ?: NewsPost::query()
+            ->whereNotNull('published_at')
+            ->orderByDesc('published_at')
+            ->first();
+
+        $heroDevice = $brand->devices()
+            ->whereNotNull('image')
+            ->orderByDesc('release_date')
+            ->first();
+
+        return view('brands.show', compact(
+            'brand',
+            'devices',
+            'deviceCount',
+            'brands',
+            'editorialPost',
+            'heroDevice',
+            'sort'
+        ));
     }
 }
