b1941602c8b2a6fe6ff473f0e4dff477145a8c74
Sinet Tork <156560309+sinettork@users.noreply.github.com>
2026-09-05 09:57:25 +0700

    Make device specification categories flexible

--- resources/views/admin/devices/_spec-row.blade.php
+++ resources/views/admin/devices/_spec-row.blade.php
@@ -7,22 +7,42 @@
     <div class="row g-3">
         <div class="col-md-4">
             <label class="form-label small">Category</label>
-            <select name="specs[{{ $index }}][category]" class="form-select form-select-sm" required>
-                <option value="">Select category</option>
-                @foreach(['Display', 'Camera', 'Battery', 'Performance', 'Body', 'Connectivity'] as $category)
-                    <option value="{{ $category }}" @selected(($spec['category'] ?? '') === $category)>{{ $category }}</option>
-                @endforeach
-            </select>
+            <input
+                type="text"
+                name="specs[{{ $index }}][category]"
+                value="{{ $spec['category'] ?? '' }}"
+                class="form-control form-control-sm"
+                list="spec-category-options"
+                placeholder="Display"
+                required
+                maxlength="100"
+            >
         </div>
 
         <div class="col-md-4">
             <label class="form-label small">Spec Name</label>
-            <input type="text" name="specs[{{ $index }}][spec_key]" value="{{ $spec['spec_key'] ?? '' }}" class="form-control form-control-sm" placeholder="Screen Size" required maxlength="255">
+            <input
+                type="text"
+                name="specs[{{ $index }}][spec_key]"
+                value="{{ $spec['spec_key'] ?? '' }}"
+                class="form-control form-control-sm"
+                placeholder="Screen Size"
+                required
+                maxlength="255"
+            >
         </div>
 
         <div class="col-md-4">
             <label class="form-label small">Value</label>
-            <input type="text" name="specs[{{ $index }}][spec_value]" value="{{ $spec['spec_value'] ?? '' }}" class="form-control form-control-sm" placeholder="6.3 inches" required maxlength="500">
+            <input
+                type="text"
+                name="specs[{{ $index }}][spec_value]"
+                value="{{ $spec['spec_value'] ?? '' }}"
+                class="form-control form-control-sm"
+                placeholder="6.3 inches"
+                required
+                maxlength="500"
+            >
         </div>
     </div>
 </div>
