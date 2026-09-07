7d401f48a7c128fa6fc9d97880ca34baf96233d4
sreyhoeurnouch-byte <sreyhoeurnouch@gmail.com>
2026-09-07 10:26:47 +0700

    Load global device-card styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import * as bootstrap from 'bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import '../css/device-cards.css';
 import '../css/compare.css';
 
 window.bootstrap = bootstrap;
