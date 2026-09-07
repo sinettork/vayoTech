931346558d63005f4f674d68a96cf77afd3011c7
sreyhoeurnouch-byte <sreyhoeurnouch@gmail.com>
2026-09-07 10:26:11 +0700

    Load Font Awesome through Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import * as bootstrap from 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../css/compare.css';
 
 window.bootstrap = bootstrap;
